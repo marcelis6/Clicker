36fe34086f92b2692d6f58661994259164fdab50
marcelis6 <123846226+marcelis6@users.noreply.github.com>
2024-01-29 22:15:21 +0100

    added doble dmg btn

--- script.js
+++ script.js
@@ -2,23 +2,30 @@ let Block = document.querySelector(".block");
 let HpCounter = document.querySelector(".hp");
 let BlocksDestroyed = document.querySelector(".destroyed");
 let BlocksDestroyedCounter = 0;
-
+let upgrade1 = document.querySelector(".doubledmg");
+let x = 1;
 const BlocksGenerator =[
      { name:"blue", hp:5, },
+     {name:"red", hp:10}
 
     ]
     
-
-
-
 Block.addEventListener("click", () => { 
-    BlocksGenerator[0].hp = BlocksGenerator[0].hp -  1;
-    HpCounter.innerHTML = `${BlocksGenerator[0].hp}`;
+    let random = Math.floor(Math.random() * BlocksGenerator.length);
 
-    if(BlocksGenerator[0].hp == 0){ 
+    BlocksGenerator[0].hp = BlocksGenerator[0].hp -  x;
+    HpCounter.innerHTML = `${BlocksGenerator[0].hp}`;
+    
+
+    if(BlocksGenerator[0].hp <= 0){ 
         BlocksDestroyedCounter += 1;
         BlocksDestroyed.innerHTML = BlocksDestroyedCounter;
         BlocksGenerator[0].hp = 5;
+        HpCounter.innerHTML = `${BlocksGenerator[0].hp}`;
     
     }
+})
+upgrade1.addEventListener("click", () => { 
+    x++;
+
 })
