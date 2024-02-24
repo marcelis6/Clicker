abac051d68eadabacaa532282ca1c7e6ecdbef9b
marcelis6 <123846226+marcelis6@users.noreply.github.com>
2024-02-24 17:20:12 +0100

    massive changes, code optimizing, added new upgrade

--- script.js
+++ script.js
@@ -1,64 +1,43 @@
-let Block = document.querySelector(".block");
-let HpCounter = document.querySelector(".hp");
-let BlocksDestroyed = document.querySelector(".destroyed");
-let BlocksDestroyedCounter = 0;
-let upgrade1 = document.querySelector(".doubledmg");
-let x = 1;
-let CashMade = document.querySelector("#CashMade");
-let Cash = 0;
+const Block = document.querySelector(".block");
+const upgrade1 = document.querySelector("#upgrade1");
+const CashMade = document.querySelector("#CashMade");
+const upgrade2 = document.querySelector("#upgrade2");
 
+let x = 1;
+let Cash = 0;
+let dps = 0;
 
 
 const BlocksGenerator =[
-     { name:"blue", hp:Math.floor(Math.random()*10) + 10, },
+     { name:"blue", },
     
 
     ]
     
 Block.addEventListener("click", () => { 
-    let random = Math.floor(Math.random() * BlocksGenerator.length);
-
-    BlocksGenerator[0].hp = BlocksGenerator[0].hp -  x;
-    HpCounter.innerHTML = `${BlocksGenerator[0].hp}`;
+   CashAdd();
     
-
-    if(BlocksGenerator[0].hp <= 0){ 
-        BlocksDestroyedCounter += 1;
-        BlocksDestroyed.innerHTML = BlocksDestroyedCounter;
-        Cash++;
-        CashMade.innerHTML = Cash;
-        BlocksGenerator[0].hp = Math.floor(Math.random()*10) + 10;
-        HpCounter.innerHTML = `${BlocksGenerator[0].hp}`;
-        FadeOut(Block);
-        setTimeout("FadeIn(Block)", 400);
-    }
 })
 upgrade1.addEventListener("click", () => { 
-    if(Cash >= 2){
+    if(Cash >= 50){
         x++;
-        Cash -= 2;
+        Cash -= 50;
         CashMade.innerHTML = Cash;
     } else alert("you don't have enough cash!");
    
 })
-
-function FadeOut(el){ 
-    let Opacity = 1;
-    let interval = setInterval(()=> { 
-        if (Opacity > 0) { 
-            Opacity -=0.1;
-            el.style.opacity = Opacity;
-        } else {
-            clearInterval(interval);          
-        }
-    }, 50)
+upgrade2.addEventListener("click", ()=> {
+    if(Cash >= 100){
+        dps++;
+        Cash -= 100;
+        CashMade.innerHTML = Cash;
+    } else alert("you don't have enough cash!");
+})
+function CashAdd(){ 
+    Cash = Cash + x;
+    CashMade.innerHTML = `${Cash}`;
 }
-function FadeIn(el){ 
-    let Opacity = 0;
-    let interval = setInterval (() =>{ 
-        if(Opacity < 1) { 
-            Opacity +=0.1;
-            el.style.opacity = Opacity;
-        } else clearInterval(interval);
-    }, 50)
-}
+setInterval(()=>{
+    Cash = Cash + dps;
+    CashMade.innerHTML = Cash;
+}, 1000)
